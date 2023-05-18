d343edd7ffb9816bf4b1b94b5b5d0eeaadaf2f09
Nisar <nisar.tahir@gmail.com>
2023-05-18 15:19:55 +0100

    Update hwinfowin.bat

--- hwinfowin.bat
+++ hwinfowin.bat
@@ -1,12 +1,17 @@
 @echo off
+
 cls
+
 echo +--------------+
 echo ^| Laptop Model ^|
 echo +--------------+
+
 wmic csproduct get name
+
 echo +---------------+
 echo ^| Laptop Serial ^|
 echo +---------------+
+
 setlocal enabledelayedexpansion
 
 set "serialNumber=undefined"
@@ -26,15 +31,20 @@ if "%serialNumber%" == "undefined" (
 ) else (
     echo Serial number: %serialNumber%
 )
+
 echo.
+
 echo +-------------+
 echo ^| Laptop BIOS ^|
 echo +-------------+
+
 wmic bios get smbiosbiosversion
+
 echo +-------------------+
 echo ^| Laptop Approx Age ^|
 echo +-------------------+
 echo.
+
 setlocal enabledelayedexpansion
 
 for /f "skip=1 tokens=2 delims==" %%G in ('wmic bios get releasedate /format:list') do (
@@ -48,19 +58,19 @@ if "%releasedate%"=="" (
 ) else (
     echo BIOS Release Date: %releasedate%
 
-    REM Extract year, month, and day from the release date
+    rem Extract year, month, and day from the release date
     set "year=!releasedate:~0,4!"
     set "month=!releasedate:~4,2!"
     set "day=!releasedate:~6,2!"
 
-    REM Get the current date
+    rem Get the current date
     for /f "tokens=1-3 delims=/." %%A in ("%date%") do (
         set "currentyear=%%C"
         set "currentmonth=%%A"
         set "currentday=%%B"
     )
 
-    REM Calculate the approximate age if release date is not empty
+    rem Calculate the approximate age if release date is not empty
     if not "!year!!month!!day!"=="" (
         set /a "age=currentyear-year"
         if !currentmonth! lss !month! set /a "age-=1"
@@ -73,9 +83,11 @@ if "%releasedate%"=="" (
 
 endlocal
 echo.
+
 echo +------------+
 echo ^| Laptop RAM ^|
 echo +------------+
+
 for /f "usebackq" %%i in (`powershell -c "(Get-WmiObject Win32_OperatingSystem).TotalVisibleMemorySize"`) do set TotalVisibleMemorySize=%%i
 echo The total visible memory size is: %TotalVisibleMemorySize% MB
 for /f "usebackq" %%i in (`powershell -c "[math]::Round((Get-WmiObject Win32_OperatingSystem).TotalVisibleMemorySize / 1MB, 2)"`) do set TotalVisibleMemorySize=%%i
@@ -83,9 +95,11 @@ echo The total visible memory size is: %TotalVisibleMemorySize% GB
 echo. 
 echo Memory SIMMs/DIMMs details:
 wmic MemoryChip get BankLabel, Capacity, MemoryType, TypeDetail, Speed
+
 echo +------------+
 echo ^| Laptop HDD ^|
 echo +------------+
+
 setlocal enabledelayedexpansion
 
 rem Run PowerShell command and store the output in a temporary file
@@ -100,6 +114,7 @@ del temp.txt
 endlocal
 
 rem powershell -c "Get-CimInstance -Class CIM_LogicalDisk | Select-Object @{Name='Size (GB)';Expression={$_.size/1gb}}, @{Name='Free Space (GB)';Expression={$_.freespace/1gb}}, @{Name='Free (%%)';Expression={' {0,6:P0}' -f(($_.freespace/1gb) / ($_.size/1gb))}}, DeviceID, DriveType | Where-Object DriveType -EQ '3'"
+
 echo.
 echo +------------+
 echo ^| Laptop CPU ^|
